8ebebaef9e095e8a426e73b8ec6be93da0874c2c
Ansh Rastogi <anshrastogi9236920967@gmail.com>
2026-07-08 21:34:15 +0545

    chore: configure Java project and enable VS Code run/debug

--- src/app/Main.java
+++ src/app/Main.java
@@ -1,5 +1,8 @@
 package app;
+import utils.ConsoleUI;
 
 public class Main{
-
+    public static void main(String[] args) {
+        ConsoleUI.showSplashScreen();
+    }
 }
--- src/utils/ConsoleUI.java
+++ src/utils/ConsoleUI.java
@@ -1,5 +1,27 @@
 package utils;
 
+import java.util.Scanner;
+
 public class ConsoleUI {
-    
+
+    public static void showSplashScreen() {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("==================================================");
+        System.out.println("                  EDUNEXUS");
+        System.out.println("==================================================");
+        System.out.println("      Smart School Management Platform            ");
+        System.out.println();
+        System.out.println("             Console Prototype v0.2               ");
+        System.out.println();
+        System.out.println(" Developed By : Ansh Rastogi                      ");
+        System.out.println("==================================================");
+        System.out.println();
+        System.out.println("Press ENTER to continue...");
+
+        scanner.nextLine();
+
+    }
+
 }
