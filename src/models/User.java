bd2ed171d2c90660cbe0d28f87355d859c9e9797
Ansh Rastogi <anshrastogi9236920967@gmail.com>
2026-07-08 22:16:13 +0545

    feat: add User model

--- src/models/User.java
+++ src/models/User.java
@@ -1,5 +1,29 @@
 package models;
 
 public class User {
-    
+
+    private String id;
+    private String name;
+    private String role;
+    private String password;
+
+    public User(String id, String name, String role, String password) {
+        this.id = id;
+        this.name = name;
+        this.role = role;
+        this.password = password;
+    }
+    public String getId() {
+        return id;
+    }
+    public String getName() {
+        return name;
+    }
+    public String getRole() {
+        return role;
+    }
+    public String getPassword() {
+        return password;
+    }
+
 }
